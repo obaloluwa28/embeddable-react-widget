087d0a94311c8fb6b30c7c2a54644489dc9b8a03
obaloluwa28 <oduyemi.obaloluwa@gmail.com>
2022-09-29 05:00:35 +0100

    added webpack build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from './logo.svg';
+// import logo from '../src/asset/paris.jpg';
 // import './App.css';
 
 function App() {
@@ -9,7 +9,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>Image Could Not Load</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
